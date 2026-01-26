5260cbc0543818ba56a5c9fb2ff1e096fc9fcb1e
Mario Ortega <mcor63@gmail.com>
2026-01-25 22:09:54 -0500

    Updating First Steps Project

---
+++ 04-first-steps/src/FirstStepsApp.tsx
@@ -0,0 +1,12 @@
+
+
+export function FirstStepsApp() {
+    return (
+        <>
+            <h1>Hola Mundo!</h1>
+            <p>Mi primer componente</p>
+            <button>Click Aquí</button>
+        </>
+    );
+
+}
---
+++ 04-first-steps/src/MyAwesomeApp.tsx
@@ -0,0 +1,9 @@
+export function MyAwesomeApp() {
+    return (
+        <>
+            <h1>Mario</h1>
+            <h3>Ortega</h3>
+        </>
+
+    );
+}
