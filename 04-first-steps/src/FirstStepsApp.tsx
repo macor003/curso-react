9efa492a4e69d1bcada274cdbaf466d72f62f88e
Mario Ortega <mcor63@gmail.com>
2026-01-29 22:50:23 -0500

    Practica de listado de elementos desde un arreglo

--- 04-first-steps/src/FirstStepsApp.tsx
+++ 04-first-steps/src/FirstStepsApp.tsx
@@ -1,13 +1,28 @@
 import { ItemCounter } from "./shopping-cart/ItemCounter";
 
+interface ItemCart {
+    productName: string;
+    quantity: number;
+}
+
+const ItemInCart: ItemCart[] = [
+    { productName: 'Nintendo DS', quantity: 1 },
+    { productName: 'Nintendo Switch 2', quantity: 2 },
+    { productName: 'Xbox Series S', quantity: 4 },
+];
 
 export function FirstStepsApp() {
     return (
         <>
             <h1>Carrito de Compras</h1>
-            <ItemCounter name="Nintendo Switch 2" quantity={0} />
+
+            {ItemInCart.map(({ productName, quantity }) => (
+                <ItemCounter key={productName} name={productName} quantity={quantity} />
+            ))}
+
+            {/* <ItemCounter name="Nintendo Switch 2" quantity={0} />
             <ItemCounter name="Xbox Series S" quantity={1} />
-            <ItemCounter name="Play Station 5" quantity={2} />
+            <ItemCounter name="Play Station 5" quantity={2} /> */}
         </>
     );
 
